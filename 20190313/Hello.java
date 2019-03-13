8fe9a6d80550d3ae9d134a71c3507403867272ef
Lee GyeongMuk <moogi3469@gmail.com>
2019-03-13 22:24:21 +0900

    Java 입문

---
+++ 20190313/Hello.java
@@ -0,0 +1,5 @@
+class Hello{    //Java에서 소스 파일의 이름과 클래스명은 같아야 한다.
+    public static void main(String[] args){ //Java에서 일반적으로 메소드는 public으로 선언해 주어야 한다.
+        System.out.println("Hello, world!");
+    }
+}
