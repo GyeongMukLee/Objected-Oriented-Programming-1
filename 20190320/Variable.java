643da757a90ed812a7b442375ca2db5d1cec8e55
Lee GyeongMuk <moogi3469@gmail.com>
2019-03-21 09:55:05 +0900

    자바 변수배움

---
+++ 20190320/Variable.java
@@ -0,0 +1,18 @@
+class Variable{
+    public static void main(String[] args){
+        //정수 타입
+        byte by = 127;
+        char ch = 65535;
+        char ch2 = 'A';  //문자도 입력할수있다
+        short sh = 32767;
+        int in = 2147483647;
+        long lo = 15454454;
+        
+        //실수 타입
+        float fl=3.4028235e38f;
+        double dou=1.7976931348623157e308;
+        
+        //논리 타입
+        boolean boo=false;
+    }
+}
