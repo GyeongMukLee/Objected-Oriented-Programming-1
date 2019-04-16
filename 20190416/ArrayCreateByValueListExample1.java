3bb126de7c7a849d41c99726dc60785542821fd6
Lee GyeongMuk <moogi3469@gmail.com>
2019-04-16 16:35:46 +0900

    배열 실습내용 추가

--- 20190416/ArrayCreateByValueListExample1.java
+++ 20190416/ArrayCreateByValueListExample1.java
@@ -1,9 +1,13 @@
 public class ArrayCreateByValueListExample1{
     public static void main(String[]args){
-        int[] arr={83,90,87};
+        int[] arr={83,90,87,92};
+        int sum=0;
+        sum=arr[0]+arr[1]+arr[2]+arr[3];
 
-        for(int i=0;i<3;i++){
+        for(int i=0;i<4;i++){
             System.out.println(arr[i]);
         }
+        System.out.println("sum: "+sum);
+        System.out.println("average: "+sum/4.0);
     }
 }
