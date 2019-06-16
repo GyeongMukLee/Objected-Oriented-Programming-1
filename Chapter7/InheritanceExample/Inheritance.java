b9f9c2f3d345c04096449bafc197ccf09b3ea070
Lee GyeongMuk <moogi3469@gmail.com>
2019-06-16 16:12:31 +0900

    Update Inheritance.java

--- Chapter7/InheritanceExample/Inheritance.java
+++ Chapter7/InheritanceExample/Inheritance.java
@@ -7,7 +7,7 @@ public class Inheritance {
         c.Eat(); // Parent가 하는 것은 Child도 할 수 있다.
 
         c.Study();
-        p.Study(); // Child는 할 수 있지만 Parent는 할 수 없다.
+        // p.Study(); // Child는 할 수 있지만 Parent는 할 수 없다.
 
         p.Walk();
         c.Walk();
